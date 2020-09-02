eb62a7e85f0d36f1ea4ec2bef9229fb2f9ed2dd3
arkiver <arkiver@protonmail.com>
2020-09-02 09:48:57 -0400

    Version 20200902.01. Support Wget-AT version 1.20.3-at.20200902.01.

--- pipeline.py
+++ pipeline.py
@@ -41,7 +41,11 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 
 WGET_AT = find_executable(
     'Wget+AT',
-    ['GNU Wget 1.20.3-at.20200401.01', 'GNU Wget 1.20.3-at.20200804.01'],
+    [
+        'GNU Wget 1.20.3-at.20200401.01',
+        'GNU Wget 1.20.3-at.20200804.01',
+        'GNU Wget 1.20.3-at.20200902.01'
+    ],
     ['./wget-at']
 )
 
@@ -54,7 +58,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20200806.01'
+VERSION = '20200902.01'
 USER_AGENT = 'mercurial/proto-1.0 (Mercurial 5.3.1)'
 TRACKER_ID = 'mercurial'
 TRACKER_HOST = 'trackerproxy.meo.ws'
